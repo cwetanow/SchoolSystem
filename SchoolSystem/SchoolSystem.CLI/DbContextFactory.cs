7438e86b1ea0f0b01ad909a06c1e17b736afd4fc
Ivan <madritistaaa@gmail.com>
2018-06-23 16:30:20 +0300

    Add db context factory for ef migrations

---
+++ SchoolSystem/SchoolSystem.CLI/DbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SchoolSystem.Data;
+using System.IO;
+
+namespace SchoolSystem.CLI
+{
+	public class DbContextFactory : IDesignTimeDbContextFactory<SchoolSystemDbContext>
+	{
+		public SchoolSystemDbContext CreateDbContext(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+						.SetBasePath(Directory.GetCurrentDirectory())
+						.AddJsonFile("appsettings.json")
+						.Build();
+
+			var connectionString = configuration.GetConnectionString("Default");
+
+			var options = SqlServerDbContextOptionsExtensions
+					.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+
+			return new SchoolSystemDbContext(options);
+		}
+	}
+}
