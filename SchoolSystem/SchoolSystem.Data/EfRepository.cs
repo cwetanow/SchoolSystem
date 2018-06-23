59d19fe09f3a5f6ad6b3f658cafbecfbc3751774
Ivan <madritistaaa@gmail.com>
2018-06-23 11:39:55 +0300

    Implement repository

---
+++ SchoolSystem/SchoolSystem.Data/EfRepository.cs
@@ -0,0 +1,44 @@
+﻿using SchoolSystem.Data.Contracts;
+using System.Linq;
+
+namespace SchoolSystem.Data
+{
+	public class EfRepository<T> : IRepository<T>
+		  where T : class
+	{
+		private readonly IDbContext dbContext;
+
+		public EfRepository(IDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public IQueryable<T> All
+		{
+			get
+			{
+				return this.dbContext.DbSet<T>();
+			}
+		}
+
+		public void Add(T entity)
+		{
+			this.dbContext.SetAdded(entity);
+		}
+
+		public void Delete(T entity)
+		{
+			this.dbContext.SetDeleted(entity);
+		}
+
+		public T GetById(object id)
+		{
+			return this.dbContext.DbSet<T>().Find(id);
+		}
+
+		public void Update(T entity)
+		{
+			this.dbContext.SetUpdated(entity);
+		}
+	}
+}
