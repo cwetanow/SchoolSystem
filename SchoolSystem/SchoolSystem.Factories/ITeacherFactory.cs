b11c0e462f379d345b871bc42fb5da47360c0119
Ivan <madritistaaa@gmail.com>
2018-06-23 14:39:58 +0300

    Add teacher factory

---
+++ SchoolSystem/SchoolSystem.Factories/ITeacherFactory.cs
@@ -0,0 +1,10 @@
+﻿using SchoolSystem.Models;
+using SchoolSystem.Models.Enums;
+
+namespace SchoolSystem.Factories
+{
+	public interface ITeacherFactory
+	{
+		Teacher CreateTeacher(string firstName, string lastName, Subject subject);
+	}
+}
