d0f2ccf53e2d736221f381e2720cac75526ac133
Ivan <madritistaaa@gmail.com>
2018-06-23 12:50:09 +0300

    Implement add student service

---
+++ SchoolSystem/SchoolSystem.Services/StudentService.cs
@@ -0,0 +1,26 @@
+﻿using SchoolSystem.Data.Contracts;
+using SchoolSystem.Models;
+using SchoolSystem.Services.Contracts;
+
+namespace SchoolSystem.Services
+{
+	public class StudentService : IStudentService
+	{
+		private readonly IRepository<Student> repository;
+		private readonly IUnitOfWork unitOfWork;
+
+		public StudentService(IRepository<Student> repository, IUnitOfWork unitOfWork)
+		{
+			this.repository = repository;
+			this.unitOfWork = unitOfWork;
+		}
+
+		public int AddStudent(Student student)
+		{
+			this.repository.Add(student);
+			this.unitOfWork.Commit();
+
+			return student.Id;
+		}
+	}
+}
