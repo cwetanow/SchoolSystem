cea6ec4998eb1fb784ef21d7c8baa80df6fab9ec
Ivan <madritistaaa@gmail.com>
2018-06-23 12:17:59 +0300

    Wire up in startup project

--- SchoolSystem/SchoolSystem.CLI/Program.cs
+++ SchoolSystem/SchoolSystem.CLI/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace SchoolSystem.CLI
+﻿using Microsoft.Extensions.Configuration;
+using Ninject;
+using SchoolSystem.Data.Contracts;
+using System.IO;
+
+namespace SchoolSystem.CLI
 {
 	public class Program
 	{
 		public static void Main()
 		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			var kernel = new StandardKernel();
+			kernel.Load(new SchoolSystemModule(configuration));
 		}
 	}
 }
