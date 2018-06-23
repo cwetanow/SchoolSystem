9f36d50896a264df0e564fb807d4f397400ab3a7
Ivan <madritistaaa@gmail.com>
2018-06-23 09:51:26 +0300

    Add rest of models

---
+++ SchoolSystem/SchoolSystem.Models/Teacher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using SchoolSystem.Models.Abstractions;
+using SchoolSystem.Models.Contracts;
+using SchoolSystem.Models.Enums;
+
+namespace SchoolSystem.Models
+{
+    public class Teacher : Person, ITeacher
+    {
+        public const int MaxStudentMarksCount = 20;
+
+        public Teacher(string firstName, string lastName, Subject subject)
+            : base(firstName, lastName)
+        {
+            this.Subject = subject;
+        }
+
+        public Subject Subject { get; set; }
+
+        public void AddMark(IStudent student, IMark mark)
+        {
+            if (student.Marks.Count >= MaxStudentMarksCount)
+            {
+                throw new ArgumentException($"The student's marks count exceed the maximum count of {MaxStudentMarksCount} marks");
+            }
+
+            student.Marks.Add(mark);
+        }
+    }
+}
