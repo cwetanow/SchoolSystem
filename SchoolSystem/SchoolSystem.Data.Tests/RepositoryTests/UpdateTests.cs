e71fcb30caee285bee82733e1b2a2037dba3936d
Ivan <madritistaaa@gmail.com>
2018-06-23 11:40:14 +0300

    Write tests for repository

---
+++ SchoolSystem/SchoolSystem.Data.Tests/RepositoryTests/UpdateTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.Data.Contracts;
+using SchoolSystem.Data.Tests.RepositoryTests.Fakes;
+
+namespace SchoolSystem.Data.Tests.RepositoryTests
+{
+  [TestFixture]
+  public class UpdateTests
+  {
+    [Test]
+    public void TestUpdate_ShouldCallDbContextSetUpdated()
+    {
+      // Arrange
+      var mockedDbContext = new Mock<IDbContext>();
+
+      var repository = new EfRepository<FakeRepositoryType>(mockedDbContext.Object);
+
+      var entity = new Mock<FakeRepositoryType>();
+
+      // Act
+      repository.Update(entity.Object);
+
+      // Assert
+      mockedDbContext.Verify(c => c.SetUpdated(entity.Object), Times.Once);
+    }
+  }
+}
