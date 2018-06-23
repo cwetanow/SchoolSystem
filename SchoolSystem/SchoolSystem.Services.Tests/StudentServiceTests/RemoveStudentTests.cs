821c56729a320c6d04a3e798394dcacdf72068c5
Ivan <madritistaaa@gmail.com>
2018-06-23 14:06:17 +0300

    Write tests for service

---
+++ SchoolSystem/SchoolSystem.Services.Tests/StudentServiceTests/RemoveStudentTests.cs
@@ -0,0 +1,86 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.Data.Contracts;
+using SchoolSystem.Models;
+using SchoolSystem.Models.Enums;
+
+namespace SchoolSystem.Services.Tests.StudentServiceTests
+{
+	[TestFixture]
+	public class RemoveStudentTests
+	{
+		[TestCase(1)]
+		public void TestRemoveStudent_ShouldCallRepositoryGetById(int id)
+		{
+			// Arrange
+			var mockedRepository = new Mock<IRepository<Student>>();
+			var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+			var service = new StudentService(mockedRepository.Object, mockedUnitOfWork.Object);
+
+			// Act
+			service.RemoveStudent(id);
+
+			// Assert
+			mockedRepository.Verify(r => r.GetById(id), Times.Once);
+		}
+
+		[TestCase(1)]
+		public void TestRemoveStudent_NoStudentFound_ShouldNotCallRepositoryDelete(int id)
+		{
+			// Arrange
+			var mockedRepository = new Mock<IRepository<Student>>();
+			var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+			var service = new StudentService(mockedRepository.Object, mockedUnitOfWork.Object);
+
+			// Act
+			service.RemoveStudent(id);
+
+			// Assert
+			mockedRepository.Verify(r => r.Delete(It.IsAny<Student>()), Times.Never);
+		}
+
+		[TestCase(1)]
+		public void TestRemoveStudent_StudentFound_ShouldCallRepositoryDelete(int id)
+		{
+			// Arrange
+			var student = new Student("Foo", "Bar", Grade.Eighth);
+
+			var mockedRepository = new Mock<IRepository<Student>>();
+			mockedRepository.Setup(r => r.GetById(It.IsAny<object>()))
+				.Returns(student);
+
+			var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+			var service = new StudentService(mockedRepository.Object, mockedUnitOfWork.Object);
+
+			// Act
+			service.RemoveStudent(id);
+
+			// Assert
+			mockedRepository.Verify(r => r.Delete(It.IsAny<Student>()), Times.Once);
+		}
+
+		[TestCase(1)]
+		public void TestRemoveStudent_StudentFound_ShouldCallUnitOfWorkCommit(int id)
+		{
+			// Arrange
+			var student = new Student("Foo", "Bar", Grade.Eighth);
+
+			var mockedRepository = new Mock<IRepository<Student>>();
+			mockedRepository.Setup(r => r.GetById(It.IsAny<object>()))
+				.Returns(student);
+
+			var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+			var service = new StudentService(mockedRepository.Object, mockedUnitOfWork.Object);
+
+			// Act
+			service.RemoveStudent(id);
+
+			// Assert
+			mockedUnitOfWork.Verify(u => u.Commit(), Times.Once);
+		}
+	}
+}
