a45b018ff30a77731b544e372878edee3b42d584
Ivan <madritistaaa@gmail.com>
2018-06-23 12:31:11 +0300

    Implement console reader writer

---
+++ SchoolSystem/SchoolSystem.Providers/ConsoleReaderWriterProvider.cs
@@ -0,0 +1,23 @@
+﻿using SchoolSystem.Providers.Contracts;
+using System;
+
+namespace SchoolSystem.Providers
+{
+	public class ConsoleReaderWriterProvider : IReader, IWriter
+	{
+		public string ReadLine()
+		{
+			return Console.ReadLine();
+		}
+
+		public void Write(string message)
+		{
+			Console.Write(message);
+		}
+
+		public void WriteLine(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
