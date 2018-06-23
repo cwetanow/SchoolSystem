89e1863d0a150d221747059007e1f692acf9529b
Ivan <madritistaaa@gmail.com>
2018-06-23 14:38:04 +0300

    Write tests for service

--- SchoolSystem/SchoolSystem.Services/Contracts/ITeacherService.cs
+++ SchoolSystem/SchoolSystem.Services/Contracts/ITeacherService.cs
@@ -1,6 +1,9 @@
-﻿namespace SchoolSystem.Services.Contracts
+﻿using SchoolSystem.Models;
+
+namespace SchoolSystem.Services.Contracts
 {
 	public interface ITeacherService
 	{
+		int AddTeacher(Teacher teacher);
 	}
 }
