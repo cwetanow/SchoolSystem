0657fcf9fd926cba86f8b36f923811aac8c5b7ce
Ivan <madritistaaa@gmail.com>
2018-06-23 14:56:39 +0300

    Implement command

---
+++ SchoolSystem/SchoolSystem.Commands/RemoveTeacherCommand.cs
@@ -0,0 +1,26 @@
+﻿using SchoolSystem.Commands.Contracts;
+using SchoolSystem.Services.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolSystem.Commands
+{
+	public class RemoveTeacherCommand : ICommand
+	{
+		private readonly ITeacherService service;
+
+		public RemoveTeacherCommand(ITeacherService service)
+		{
+			this.service = service ?? throw new ArgumentNullException("service cannot be null");
+		}
+
+		public string Execute(IList<string> parameters)
+		{
+			var teacherId = int.Parse(parameters[0]);
+
+			this.service.RemoveTeacher(teacherId);
+
+			return $"Teacher with ID {teacherId} was sucessfully removed.";
+		}
+	}
+}
