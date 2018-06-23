efc5f162f4579efec71a456cae637f22fe6e3213
Ivan <madritistaaa@gmail.com>
2018-06-23 14:10:25 +0300

    Write tests

---
+++ SchoolSystem/SchoolSystem.Commands.Tests/RemoveStudentCommandTests/ExecuteTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.Services.Contracts;
+
+namespace SchoolSystem.Commands.Tests.RemoveStudentCommandTests
+{
+	[TestFixture]
+	public class ExecuteTests
+	{
+		[TestCase(1)]
+		[TestCase(12)]
+		[TestCase(1123123)]
+		public void TestExecute_PassValidId_ShouldCallServiceRemoveStudentCorrectly(int id)
+		{
+			var parameters = new string[] { id.ToString() };
+
+			var mockedService = new Mock<IStudentService>();
+
+			var command = new RemoveStudentCommand(mockedService.Object);
+
+			command.Execute(parameters);
+
+			mockedService.Verify(s => s.RemoveStudent(id), Times.Once);
+		}
+	}
+}
