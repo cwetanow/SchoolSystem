f11da08b19d66766b04ed9964cf7a417e36c1f6e
Ivan <madritistaaa@gmail.com>
2018-06-23 14:26:20 +0300

    Test command

--- SchoolSystem/SchoolSystem.Commands.Tests/RemoveStudentCommandTests/ExecuteTests.cs
+++ SchoolSystem/SchoolSystem.Commands.Tests/RemoveStudentCommandTests/ExecuteTests.cs
@@ -12,14 +12,17 @@ namespace SchoolSystem.Commands.Tests.RemoveStudentCommandTests
 		[TestCase(1123123)]
 		public void TestExecute_PassValidId_ShouldCallServiceRemoveStudentCorrectly(int id)
 		{
+			// Arrange
 			var parameters = new string[] { id.ToString() };
 
 			var mockedService = new Mock<IStudentService>();
 
 			var command = new RemoveStudentCommand(mockedService.Object);
 
+			// Act
 			command.Execute(parameters);
 
+			// Assert
 			mockedService.Verify(s => s.RemoveStudent(id), Times.Once);
 		}
 	}
---
+++ SchoolSystem/SchoolSystem.Commands.Tests/StudentListMarksCommandTests/ExecuteTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.Models;
+using SchoolSystem.Models.Enums;
+using SchoolSystem.Services.Contracts;
+
+namespace SchoolSystem.Commands.Tests.StudentListMarksCommandTests
+{
+	[TestFixture]
+	public class ExecuteTests
+	{
+		[TestCase(1)]
+		public void TestExecute_ShouldCallServiceGetStudentById(int id)
+		{
+			// Arrange
+			var parameters = new string[] { id.ToString() };
+
+			var student = new Student("Foo", "Bar", Grade.Eighth);
+
+			var mockedService = new Mock<IStudentService>();
+			mockedService.Setup(s => s.GetStudentById(It.IsAny<int>()))
+				.Returns(student);
+
+			var command = new StudentListMarksCommand(mockedService.Object);
+
+			// Act
+			command.Execute(parameters);
+
+			// Assert
+			mockedService.Verify(s => s.GetStudentById(id), Times.Once);
+		}
+	}
+}
