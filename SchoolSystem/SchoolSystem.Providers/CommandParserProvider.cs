8d9a252c01f2d0b9f2fdc4923273afed09fdb96a
Ivan <madritistaaa@gmail.com>
2018-06-23 12:27:06 +0300

    Implement command parser

---
+++ SchoolSystem/SchoolSystem.Providers/CommandParserProvider.cs
@@ -0,0 +1,41 @@
+﻿using SchoolSystem.Commands;
+using SchoolSystem.Commands.Contracts;
+using SchoolSystem.Providers.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolSystem.Providers
+{
+	public class CommandParserProvider : IParser
+	{
+		private readonly ICommandFactory factory;
+
+		public CommandParserProvider(ICommandFactory factory)
+		{
+			this.factory = factory;
+		}
+
+		public ICommand ParseCommand(string fullCommand)
+		{
+			var commandName = fullCommand.Split(' ')[0];
+
+			var command = this.factory.CreateCommand(commandName);
+
+			return command;
+		}
+
+		public IList<string> ParseParameters(string fullCommand)
+		{
+			var commandParts = fullCommand.Split(' ')
+				.ToList();
+			commandParts.RemoveAt(0);
+
+			if (!commandParts.Any())
+			{
+				return null;
+			}
+
+			return commandParts;
+		}
+	}
+}
