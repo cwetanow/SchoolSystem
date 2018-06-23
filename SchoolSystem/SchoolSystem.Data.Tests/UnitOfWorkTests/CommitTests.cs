ac010ac79763f4367e6308f011d0b8534175940e
Ivan <madritistaaa@gmail.com>
2018-06-23 11:36:50 +0300

    Add unit of work implementation with tests

---
+++ SchoolSystem/SchoolSystem.Data.Tests/UnitOfWorkTests/CommitTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.Data.Contracts;
+
+namespace SchoolSystem.Data.Tests.UnitOfWorkTests
+{
+	[TestFixture]
+	public class CommitTests
+	{
+		[Test]
+		public void TestCommit_ShouldCallDbContextSaveChanges()
+		{
+			// Arrange
+			var mockedDbContext = new Mock<IDbContext>();
+
+			var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+			// Act
+			unitOfWork.Commit();
+
+			// Assert
+			mockedDbContext.Verify(c => c.SaveChanges(), Times.Once);
+		}
+
+		[TestCase(1)]
+		[TestCase(723)]
+		[TestCase(14)]
+		public void TestCommit_ShouldReturnCorrectly(int expectedResult)
+		{
+			// Arrange
+			var mockedDbContext = new Mock<IDbContext>();
+			mockedDbContext.Setup(d => d.SaveChanges()).Returns(expectedResult);
+
+			var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+			// Act
+			var result = unitOfWork.Commit();
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
---
+++ SchoolSystem/SchoolSystem.Data/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using SchoolSystem.Data.Contracts;
+
+namespace SchoolSystem.Data
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly IDbContext dbContext;
+
+		public UnitOfWork(IDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public int Commit()
+		{
+			return this.dbContext.SaveChanges();
+		}
+	}
+}
